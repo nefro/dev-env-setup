6495fe9f016824718167e3745e7b86a82e376202
mario <mariusz.sygnowski@gmail.com>
2021-04-22 01:13:42 +0200

    proper maven mount path

--- spring-data-flow/scdf.sh
+++ spring-data-flow/scdf.sh
@@ -1,6 +1,6 @@
 export DATAFLOW_VERSION=2.7.1
 export SKIPPER_VERSION=2.6.1
-export HOST_MOUNT_PATH=~/.m2/repository/
+export HOST_MOUNT_PATH=~/.m2
 export DOCKER_MOUNT_PATH=/root/.m2
 
 docker-compose up -d
